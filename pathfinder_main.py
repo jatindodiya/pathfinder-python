da65929aa30082d076239acc743ff82f4b0a15fa
jatin dodiya <48292361+jatindodiya@users.noreply.github.com>
2020-10-27 16:57:45 +0530

    Update pathfinder_main.py

--- pathfinder_main.py
+++ pathfinder_main.py
@@ -5,6 +5,10 @@ import os
 # -----------------------------------color define-----------------------------------------
 red = (178, 34, 34)
 orange = (255, 110, 0)
+blue = (30, 144, 255)
+pink = (255, 20, 147)
+black = (0, 0, 0)
+aqua = (0, 255, 255)
 # -----------------------------------------------------------------------------------------
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (50, 50)
 root = Tk()
@@ -17,9 +21,9 @@ pygame.init()
 pygame.display.set_caption("Path Finder Algorithm visualisation")
 clock = pygame.time.Clock()
 font = pygame.font.SysFont('comicsans', 30, True)
-bg = pygame.image.load('photos/bg.jpg')
+#bg = pygame.image.load('photos/bg.jpg')
 mainScreen = pygame.display.set_mode([screen_width, screen_height])  # main screen object and final setup
-mainScreen.blit(bg, (0, 0))
+#mainScreen.blit(bg, (0, 0))
 
 
 class rect(object):
@@ -38,8 +42,10 @@ class rect(object):
         self.color = color
         pygame.display.update()
 
-    def start(self):
-        pass
+    def start(self, color):
+        self.color = color
+        pygame.draw.rect(mainScreen, self.color, (self.x, self.y, self.width, self.height))
+        pygame.display.update()
 
     def end(self):
         pass
@@ -49,10 +55,12 @@ class rect(object):
 
 
 def onsubmit():
-    global st
-    global ed
+    #global st
+    #global ed
     st = startBox.get().split(',')
     ed = endBox.get().split(',')
+    spots[int(st[0])][int(st[1])].start(orange)
+    spots[int(ed[0])][int(ed[1])].start(blue)
     print(st)
     print(ed)
     root.quit()
@@ -65,11 +73,11 @@ def displayMessage(message, fontSize, height):
     mainScreen.blit(text, (200, height))
     pygame.display.update()
 
-# ----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------
 # this is space for grid spot
 
 
-sizeofarr = 7
+sizeofarr = 50
 spotHeight = screen_height // sizeofarr
 print(spotHeight)
 spotWidth = screen_width // sizeofarr
@@ -78,7 +86,7 @@ print(spotWidth)
 spots = [[0 for i in range(sizeofarr)] for j in range(sizeofarr)]
 for i in range(sizeofarr):
     for j in range(sizeofarr):
-        spots[i][j] = rect(i * spotWidth, j * spotHeight, spotWidth - 1, spotHeight - 1)
+        spots[i][j] = rect(i * spotWidth, j * spotHeight, spotWidth -0.5, spotHeight-0.5)
 
 for row in spots:
     for spot in row:
@@ -112,14 +120,10 @@ def mydelay(miliSec):  # this function we created gives appprox delay in milisec
                 pygame.quit()
 
 
-def main():
-    mydelay(100)
-    pygame.display.update()
-    spots[1][2].changecolor(orange)
-
-
 frame.pack()
 root.mainloop()
 run = True
 while run:
-    main()
+    mydelay(100)
+    pygame.display.update()
+
